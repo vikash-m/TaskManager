ed9a0fa16e2247257f9a904d4968b20b4cddc8a4
SHUBRATHAC-RM\shubr <shubratha@infrrd.ai>
2017-06-05 15:56:56 +0530

    UserInfoVm

---
+++ TaskManager/TaskManager/ViewModels/UserInfoVm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManager.ViewModels
+{
+	public class UserInfoVm
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public long PhoneNumber { get; set; }
+		public string EmailId { get; set; }
+		public string RoleName { get; set; }
+	}
+}
