700fa6b4934eb82294d7ea0ddeb32b11a8d674c4
PRANAV-PC\sha zaffer <zaffar@infrrd.ai>
2017-06-06 11:00:34 +0530

    Login Module And Login View

---
+++ TaskManager/TaskDomain/DomainModel/CustomRole.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskDomain.DomainModel
+{
+    class CustomRole
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        //public CustomRole()
+        //{
+        //    this.LoginUsers = new HashSet<CustomLoginUser>();
+        //    this.Userdetails = new HashSet<CustomUserDetails>();
+        //}
+
+        public long RoleId { get; set; }
+        public string RoleName { get; set; }
+        public System.DateTime CreateDate { get; set; }
+        public Nullable<System.DateTime> ModifiedDate { get; set; }
+        public bool IsDeleted { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+       // public virtual ICollection<CustomLoginUser> LoginUsers { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+     //   public virtual ICollection<CustomUserDetails> Userdetails { get; set; }
+
+    }
+}
