555820784e9796633b5c1f40848ea5cc68173bab
shalini <shalini@infrrd.ai>
2017-06-13 15:37:50 +0530

    changes

---
+++ TaskManager/TaskDomain/DomainModel/RoleModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskDomain.DomainModel
+{
+    public class RoleModel
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+    }
+}
