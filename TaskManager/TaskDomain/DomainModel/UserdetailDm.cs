b5dac676912cf3e8e3ae382b1bcb0d5544ee8dcb
shalini <shalini@infrrd.ai>
2017-06-30 12:29:34 +0530

    changes in save user details

--- TaskManager/TaskDomain/DomainModel/UserdetailDm.cs
+++ TaskManager/TaskDomain/DomainModel/UserdetailDm.cs
@@ -15,7 +15,8 @@ namespace TaskDomain.DomainModel
         [Display(Name = "Phone no")]
         [Required(ErrorMessage = "Enter the Phone no")]
         [DataType(DataType.PhoneNumber)]
-        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid Phone number")]
+        //[RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid Phone number")]
+
         public string PhoneNumber { get; set; }
         [Display(Name = "Email")]
         [Required(ErrorMessage = "Enter Email Id")]
