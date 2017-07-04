94e180abd805ccf5280e995215dda1773e162599
swatikharpor <skharpor17@gmail.com>
2017-07-04 16:37:39 +0530

    changes

--- TaskManagerDAL/TaskManagerDAL/DAL/AdminRepository.cs
+++ TaskManagerDAL/TaskManagerDAL/DAL/AdminRepository.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using TaskManagerDAL.Models;
 using TaskDomain.DomainModel;
+using TaskManagerDAL.Models;
 
 namespace TaskManagerDAL.DAL
 {
@@ -17,58 +16,71 @@ namespace TaskManagerDAL.DAL
             var num = _taskManagerEntities.SaveChanges();
             return num > 0;
         }
+
         public List<UserDetailDm> GetUser()
         {
-            return _taskManagerEntities.UserDetails.Where(m => m.IsDeleted != true).ToList().Select(userDetailDm => new UserDetailDm
-            {
-                Id = userDetailDm.Id,
-                FirstName = userDetailDm.FirstName,
-                LastName = userDetailDm.LastName,
-                EmailId = userDetailDm.EmailId,
-                PhoneNumber = userDetailDm.PhoneNumber,
-                RoleId = userDetailDm.Role.RoleId,
-                ManagerId = userDetailDm.ManagerId,
-                ModifiedBy = userDetailDm.ModifiedBy,
-                CreateDate = DateTime.Now,
-                ModifiedDate = DateTime.Now,
-                ManagerName = GetManagerNameById(userDetailDm.ManagerId)
-            }).ToList();
+            return
+                _taskManagerEntities.UserDetails.Where(m => m.IsDeleted != true)
+                    .ToList()
+                    .Select(userDetailDm => new UserDetailDm
+                    {
+                        Id = userDetailDm.Id,
+                        FirstName = userDetailDm.FirstName,
+                        LastName = userDetailDm.LastName,
+                        EmailId = userDetailDm.EmailId,
+                        PhoneNumber = userDetailDm.PhoneNumber,
+                        RoleId = userDetailDm.Role.RoleId,
+                        ManagerId = userDetailDm.ManagerId,
+                        ModifiedBy = userDetailDm.ModifiedBy,
+                        CreateDate = DateTime.Now,
+                        ModifiedDate = DateTime.Now,
+                        ManagerName = GetManagerNameById(userDetailDm.ManagerId)
+                    }).ToList();
         }
+
         public bool CreateLoginUser(LoginUser loginUser)
         {
             _taskManagerEntities.LoginUsers.Add(loginUser);
             var saveStatus = _taskManagerEntities.SaveChanges();
             return saveStatus > 0;
         }
+
         public List<RoleDm> GetRoles() => _taskManagerEntities.Roles.ToList().Select(roles => new RoleDm
         {
             RoleId = roles.RoleId,
             RoleName = roles.RoleName
         }).ToList();
+
         public string GetRoleNameById(int roleId)
         {
             return _taskManagerEntities.Roles.FirstOrDefault(m => m.RoleId == roleId)?.RoleName;
         }
+
         public List<UserDetailDm> GetManagerByRoleId()
         {
-            var roleId = Convert.ToInt32( EnumClass.Roles.Manager);
-            var result=_taskManagerEntities.UserDetails.Where(m => m.RoleId == roleId).ToList();
-            List<UserDetailDm> userDetailList = new List<UserDetailDm>();
+            var roleId = Convert.ToInt32(EnumClass.Roles.Manager);
+            var result = _taskManagerEntities.UserDetails.Where(m => m.RoleId == roleId).ToList();
+            var userDetailList = new List<UserDetailDm>();
             foreach (var item in result)
             {
-                UserDetailDm userDetailDm = new UserDetailDm();
-                userDetailDm.FirstName = item.FirstName;
-                userDetailDm.Id = item.Id;
+                var userDetailDm = new UserDetailDm()
+                {
+                    FirstName = item.FirstName,
+                    Id = item.Id
+                };
                 userDetailList.Add(userDetailDm);
             }
-           return userDetailList;
+            return userDetailList;
         }
+
         public string GetManagerNameById(string managerId)
         {
             var result = _taskManagerEntities.UserDetails.FirstOrDefault(m => m.Id == managerId)?.FirstName;
             return result;
         }
-        public UserDetail GetUserDetailById(string id) => _taskManagerEntities.UserDetails.FirstOrDefault(x => x.Id == id);
+
+        public UserDetail GetUserDetailById(string id)
+            => _taskManagerEntities.UserDetails.FirstOrDefault(x => x.Id == id);
 
         public bool UpdateUserDetail(UserDetail userdetail)
         {
@@ -76,9 +88,7 @@ namespace TaskManagerDAL.DAL
             {
                 var userDetail = _taskManagerEntities.UserDetails.FirstOrDefault(m => m.Id == userdetail.Id);
                 if (userDetail == null)
-                {
                     return false;
-                }
                 userDetail.Id = userdetail.Id;
                 userDetail.FirstName = userdetail.FirstName;
                 userDetail.LastName = userdetail.LastName;
@@ -90,24 +100,25 @@ namespace TaskManagerDAL.DAL
                 _taskManagerEntities.SaveChanges();
                 return true;
             }
-            catch 
+            catch
             {
                 return false;
             }
         }
+
         public bool DeleteUser(string id)
         {
             var userDetail = _taskManagerEntities.UserDetails.Find(id);
 
             if (userDetail == null)
-            {
                 return false;
-            }
             userDetail.IsDeleted = true;
             userDetail.ModifiedDate = DateTime.Now;
             _taskManagerEntities.SaveChanges();
             return true;
         }
-        public UserDetail GetUserDetailByEmailId(string emailId) => _taskManagerEntities.UserDetails.First(r => r.EmailId == emailId);
+
+        public UserDetail GetUserDetailByEmailId(string emailId)
+            => _taskManagerEntities.UserDetails.First(r => r.EmailId == emailId);
     }
 }
