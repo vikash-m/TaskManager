b86b389bec520fbaa35f285ff28dd3ee6c3beac5
PRANAV-PC\sha zaffer <zaffar@infrrd.ai>
2017-07-11 16:03:34 +0530

    custom color

---
+++ TaskManagerDAL/TaskManagerDAL/CustomException/TaskManagerException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManagerDAL.CustomException
+{
+    public class TaskManagerException:Exception
+    {
+        public TaskManagerException():base()
+        {
+
+        }
+        public TaskManagerException(string message) : base(message)
+        {
+
+        }
+    }
+}
