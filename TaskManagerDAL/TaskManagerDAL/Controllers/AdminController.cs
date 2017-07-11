c0245efb0b72bc677b1f842ffe6dbc47970b52d1
PRANAV-PC\sha zaffer <zaffar@infrrd.ai>
2017-07-11 15:02:10 +0530

    Custom exception on Dal layer

--- TaskManagerDAL/TaskManagerDAL/Controllers/AdminController.cs
+++ TaskManagerDAL/TaskManagerDAL/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Web.Http;
 using TaskDomain.DomainModel;
+using TaskManagerDAL.CustomException;
 using TaskManagerDAL.DAL;
 using TaskManagerDAL.Models;
 
@@ -17,17 +18,63 @@ namespace TaskManagerDAL.Controllers
         [HttpPost, Route("create-user")]
         public bool CreateUser(UserDetail userDetail)
         {
-            return _adminRepository.CreateUser(userDetail);
+            try
+            {
+
+                try
+                {
+                    return _adminRepository.CreateUser(userDetail);
+                }
+                catch
+                {
+                    throw new TaskManagerException("Error at Creating User method of Admin controller in DAL layer.");
+                }
+            }
+            catch (TaskManagerException taskManagerException)
+            {
+                logger.Error(taskManagerException, taskManagerException.Message);
+                return false;
+            }
         }
         [HttpGet, Route("user-detail")]
         public List<UserDetailDm> GetUserDetail()
         {
-            return _adminRepository.GetUser();
+            try
+            {
+                try
+                {
+                    return _adminRepository.GetUser();
+                }
+                catch
+                {
+                    throw new TaskManagerException("Error at Listing UserDetails method of Admin controller in DAL layer.");
+                }
+            }
+            catch (TaskManagerException taskManagerException)
+            {
+                logger.Error(taskManagerException, taskManagerException.Message);
+                return null;
+            }
         }
         [HttpGet, Route("roles/{roleId}")]
         public string GetRoleById(int roleId)
         {
-            return _adminRepository.GetRoleNameById(roleId);
+            try
+            {
+                try
+                {
+                    return _adminRepository.GetRoleNameById(roleId);
+                }
+                catch
+                {
+                    throw new TaskManagerException("Error at Roles of Admin controller in DAL layer.");
+                }
+            }
+            catch (TaskManagerException taskManagerException)
+            {
+                logger.Error(taskManagerException, taskManagerException.Message);
+                return null;
+            }
         }
         [HttpPost, Route("login-user")]
         public bool CreateLoginUser(UserLoginDetails loginDetails)
@@ -36,63 +83,191 @@ namespace TaskManagerDAL.Controllers
             var employee = _adminRepository.GetUserDetailByEmailId(loginDetails.EmailId);
             try
             {
-                var loginUserDetails = new LoginUser
+                try
                 {
-                    Id = Guid.NewGuid().ToString(),
-                    RoleId = employee.RoleId,
-                    EmpId = employee.Id,
-                    UserName = employee.EmailId,
-                    Password = loginDetails.Password,
-                    CreateDate = DateTime.Now
+                    var loginUserDetails = new LoginUser
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        RoleId = employee.RoleId,
+                        EmpId = employee.Id,
+                        UserName = employee.EmailId,
+                        Password = loginDetails.Password,
+                        CreateDate = DateTime.Now
 
-                };
-                var result = _adminRepository.CreateLoginUser(loginUserDetails);
-                return result;
+                    };
+                    var result = _adminRepository.CreateLoginUser(loginUserDetails);
+                    return result;
+                }
+                catch
+                {
+                    throw new TaskManagerException("Error at Login By Email method of Admin controller in DAL layer.");
+                }
             }
-            catch (Exception ex)
+            catch (TaskManagerException taskManagerException)
             {
-                logger.Error(ex, "Error Occured");
+                logger.Error(taskManagerException, taskManagerException.Message);
                 return false;
             }
         }
         [HttpGet, Route("roles")]
         public List<RoleDm> GetRoles()
         {
-            return _adminRepository.GetRoles();
+            try {
+                try
+                {
+
+                    return _adminRepository.GetRoles();
+                }
+                catch
+                {
+                    throw new TaskManagerException("Error at Listing Roles method of Admin controller in DAL layer.");
+                }
+            }
+            catch (TaskManagerException taskManagerException)
+            {
+                logger.Error(taskManagerException, taskManagerException.Message);
+                return null;
+            }
         }
         public string GetUserDetailsAddedBy(string id)
         {
-            return _adminRepository.GetManagerNameById(id);
+            try {
+                try
+                {
+                    return _adminRepository.GetManagerNameById(id);
+                }
+                catch
+                {
+                    throw new TaskManagerException("Error at fetching UserDetail (AddedBy) method of Admin controller in DAL layer.");
+                }
+            }
+            catch (TaskManagerException taskManagerException)
+            {
+                logger.Error(taskManagerException, taskManagerException.Message);
+                return null;
+            }
         }
         [HttpGet, Route("manager")]
         public List<UserDetailDm> GetManagerByRoleId()
         {
-            return _adminRepository.GetManagerByRoleId();
+            try
+            {
+
+                try
+                {
+                    return _adminRepository.GetManagerByRoleId();
+                }
+                catch
+                {
+                    throw new TaskManagerException("Error at ManagerById method of Admin controller in DAL layer.");
+                }
+            }
+            catch (TaskManagerException taskManagerException)
+            {
+                logger.Error(taskManagerException, taskManagerException.Message);
+                return null;
+            }
         }
         public string GetManagerNameById(string managerId)
         {
-            return _adminRepository.GetManagerNameById(managerId);
+            try {
+                try
+                {
+
+
+                    return _adminRepository.GetManagerNameById(managerId);
+                }
+                catch
+                {
+                    throw new TaskManagerException("Error at ManagerNameBy Id method of Admin controller in DAL layer.");
+                }
+            }
+            catch (TaskManagerException taskManagerException)
+            {
+                logger.Error(taskManagerException, taskManagerException.Message);
+                return null;
+            }
         }
         [HttpPut, Route("{id}")]
         public bool UpdateUserDetail(UserDetail userDetail)
         {
-            return _adminRepository.UpdateUserDetail(userDetail);
+            try {
+                try
+                {
+                    return _adminRepository.UpdateUserDetail(userDetail);
+                }
+                catch
+                {
+                    throw new TaskManagerException("Error at Updating UserDetails method of Admin controller in DAL layer.");
+                }
+            }
+            catch (TaskManagerException taskManagerException)
+            {
+                logger.Error(taskManagerException, taskManagerException.Message);
+                return false;
+            }
         }
         [HttpDelete, Route("{id}")]
         public bool DeleteUser(string id, string loginUser)
         {
-            return _adminRepository.DeleteUser(id);
+            try
+            {
+
+                try
+                {
+                    return _adminRepository.DeleteUser(id);
+                }
+                catch
+                {
+                    throw new TaskManagerException("Error at deleting User method of Admin controller in DAL layer.");
+                }
+            }
+            catch (TaskManagerException taskManagerException)
+            {
+                logger.Error(taskManagerException, taskManagerException.Message);
+                return false;
+            }
         }
         [HttpGet, Route("{employeeId}")]
         public UserDetail GetUserDetailById(string employeeId)
         {
-            return _adminRepository.GetUserDetailById(employeeId);
+            try
+            {
+
+                try
+                {
+                    return _adminRepository.GetUserDetailById(employeeId);
+                }
+                catch
+                {
+                    throw new TaskManagerException("Error at UserDetailBy method of Admin controller in DAL layer.");
+                }
+            }
+            catch (TaskManagerException taskManagerException)
+            {
+                logger.Error(taskManagerException, taskManagerException.Message);
+                return null;
+            }
         }
 
         [HttpGet, Route("email")]
         public bool CheckForEmail(string emailId)
         {
-            return _adminRepository.CheckForEmail(emailId);
+            try {
+                try
+                {
+                    return _adminRepository.CheckForEmail(emailId);
+                }
+                catch
+                {
+                    throw new TaskManagerException("Error at Email method of Admin controller in DAL layer.");
+                }
+            }
+            catch (TaskManagerException taskManagerException)
+            {
+                logger.Error(taskManagerException, taskManagerException.Message);
+                return false;
+            }
         }
 
     }
--- TaskManagerDAL/TaskManagerDAL/Controllers/LoginController.cs
+++ TaskManagerDAL/TaskManagerDAL/Controllers/LoginController.cs
@@ -1,5 +1,7 @@
-﻿using System.Web.Http;
+﻿using NLog;
+using System.Web.Http;
 using TaskDomain.DomainModel;
+using TaskManagerDAL.CustomException;
 using TaskManagerDAL.DAL;
 using TaskManagerDAL.Models;
 
@@ -10,23 +12,65 @@ namespace TaskManagerDAL.Controllers
     {
         private readonly LoginRepository _loginRepository = new LoginRepository();
         private readonly RolesRepository _rolesRepository = new RolesRepository();
-
+        Logger logger = LogManager.GetCurrentClassLogger();
         [HttpGet, Route("")]
         public LoginUserDm GetLoginUserDetail(string name, string password)
         {
-            return _loginRepository.GetLoginUserDetails(name, password);
+            try {
+                try
+                {
+                    return _loginRepository.GetLoginUserDetails(name, password);
+                }
+                catch
+                {
+                    throw new TaskManagerException("Error at Login UserDetail method of Login controller in DAL layer.");
+                }
+            }
+            catch (TaskManagerException taskManagerException)
+            {
+                logger.Error(taskManagerException, taskManagerException.Message);
+                return null;
+            }
         }
 
         [HttpGet, Route("user/{id}")]
         public UserDetailDm GetUserDetailsData(string id)
         {
-            return _loginRepository.GetUserDetailsData(id);
+            try {
+                try
+                {
+                    return _loginRepository.GetUserDetailsData(id);
+                }
+                catch
+                {
+                    throw new TaskManagerException("Error at Login UserDetailData method of Login controller in DAL layer.");
+                }
+            }
+            catch (TaskManagerException taskManagerException)
+            {
+                logger.Error(taskManagerException, taskManagerException.Message);
+                return null;
+            }
         }
 
         [HttpGet, Route("roles/{id}")]
         public string GetRoleByRoleId(int id)
         {
-            return _rolesRepository.GetRoleNameById(id);
+            try {
+                try
+                {
+                    return _rolesRepository.GetRoleNameById(id);
+                }
+                catch
+                {
+                    throw new TaskManagerException("Error at Role by RoleId method of Login controller in DAL layer.");
+                }
+            }
+            catch (TaskManagerException taskManagerException)
+            {
+                logger.Error(taskManagerException, taskManagerException.Message);
+                return null;
+            }
         }
 
         [HttpGet, Route("verify")]
