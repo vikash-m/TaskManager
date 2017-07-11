2322c212133a4c80018f264763d9e6dd8f595c8b
vikash-m <vikash.mahala@raremile.com>
2017-07-11 12:24:43 +0530

    Employee Exceptions

--- TaskManagerDAL/TaskManagerDAL/Controllers/EmployeeController.cs
+++ TaskManagerDAL/TaskManagerDAL/Controllers/EmployeeController.cs
@@ -3,6 +3,9 @@ using System.Web.Http;
 using TaskManagerDAL.Models;
 using TaskManagerDAL.DAL;
 using TaskDomain.DomainModel;
+using TaskDomain.DomainModel.CustomExceptions;
+using System;
+using NLog;
 
 namespace TaskManagerDAL.Controllers
 {
@@ -10,7 +13,7 @@ namespace TaskManagerDAL.Controllers
     public class EmployeeController : ApiController
     {
         private readonly EmployeeRepository _employeeRepository = new EmployeeRepository();
-        
+        Logger logger = LogManager.GetCurrentClassLogger();
         [HttpGet, Route("")]
         public List<UserDetail> GetEmployee()
         {
@@ -26,25 +29,88 @@ namespace TaskManagerDAL.Controllers
         [HttpGet, Route("UpdateTask")]
         public bool UpdateTaskStatus(string id, int status)
         {
-            return _employeeRepository.UpdateTaskStatus(id, status);
+            try
+            {
+                try
+                {
+                    return _employeeRepository.UpdateTaskStatus(id, status);
+                }
+                catch
+                {
+                    throw new UpdateTaskStatusException("Error at UpdateTaskStatus method of Employee controller in DAL layer.");
+                }
+            }
+            catch (UpdateTaskStatusException updateTaskStatusException)
+            {
+                logger.Error(updateTaskStatusException, updateTaskStatusException.Message);
+                return false;
+            }
         }
         
         [HttpGet, Route("{employeeId}/tasks/count")]
-        public int GetTaskCount(string employeeId, int statusId)
+        public int? GetTaskCount(string employeeId, int statusId)
         {
-            return _employeeRepository.GetTaskCount(employeeId, statusId);
+            try
+            { 
+               try
+               {
+                    return _employeeRepository.GetTaskCount(employeeId, statusId);
+               }
+               catch
+               {
+                   throw new DashboardTaskCountException("Error at GetTaskCount method of Employee controller in DAL layer.");
+               }
+            }
+            catch(DashboardTaskCountException dashboardTaskCountException)
+            {
+                logger.Error(dashboardTaskCountException, dashboardTaskCountException.Message);
+                return null;
+            }
         }
 
         [HttpGet, Route("{employeeId}/tasks")]
         public List<TaskDm> GetEmployeeTask(string employeeId)
         {
-            return _employeeRepository.GetEmployeeTask(employeeId);
+            try
+            {
+                try
+                {
+
+                    return _employeeRepository.GetEmployeeTask(employeeId);
+                }
+                catch
+                {
+                    throw new EmployeeTaskException("Error at GetEmployeeTask method of Employee controller in DAL layer.");
+                }
+            }
+            catch (EmployeeTaskException employeeTaskException)
+            {
+                logger.Error(employeeTaskException, employeeTaskException.Message);
+                return null;
+            }
+            
         }
 
         [HttpGet, Route("{taskId}")]
         public TaskDm GetTaskDetail(string taskId)
         {
-            return _employeeRepository.GetTaskDetail(taskId);
+            try
+            {
+               try
+               {
+               
+                  return _employeeRepository.GetTaskDetail(taskId);
+               }
+               catch
+               {
+                throw new TaskDetailException("Error at GetTaskDetail method of Employee controller in DAL layer.");
+               }
+            }
+            catch (TaskDetailException taskDetailException)
+            {
+                logger.Error(taskDetailException, taskDetailException.Message);
+                return null;
+            }
         }
     }
 
